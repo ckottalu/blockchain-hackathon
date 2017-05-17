a29de44b8423a74c063d8c33339629a92055fe53
Chandrasekhar Kottaluru <ckottalu@oradev.oraclecorp.com>
2017-05-17 00:11:14 -0700

    change

--- chaincode/project_chaincode.go
+++ chaincode/project_chaincode.go
@@ -9,6 +9,8 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"strings"
+	"strconv"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
@@ -90,6 +92,10 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 	// Handle different functions
 	if function == "init" { //initialize the chaincode state, used as reset
 		return t.Init(stub, "init", args)
+	}else if function == "resourcetimeentry" {
+		return t.EnterResourceTime(stub,args)
+	}else if function == "completeprojectmilestone" {
+		return t.CompleteProjectMilestone(stub,args)
 	}
 
 	fmt.Println("invoke did not find func: " + function) //error
@@ -114,6 +120,7 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 	return nil, errors.New("Received unknown function query: " + function)
 }
 
+// test method to return the keys and read values
 func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 	var key, jsonResp string
 	var err error
@@ -135,7 +142,7 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) 
 //Initilizing project Data
 
 func (t *SimpleChaincode) initializeData(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	//Initilizing the sample projects
+	//Initilizing the sample projects (can be dynamically derived from DB in realtime)
 	consultingProjects := []string{"Proj1", "Proj2", "Proj3"}
 
 	jsonAsBytes, _ := json.Marshal(consultingProjects)
@@ -144,11 +151,11 @@ func (t *SimpleChaincode) initializeData(stub shim.ChaincodeStubInterface, args 
 		return nil, err
 	}
 
-	//Initilizing the Project user rates p1 -->[ {u1 100}, {u2 200 }]
+	//Initilizing the Project user rates (can be dynamically derived from DB in realtime) p1 -->[ {u1 100}, {u2 200 }]
 	var projectUserRates []UserRate
 	userrate := UserRate{}
 	userrate.User = "Chandra"
-	userrate.Rate = "90"
+	userrate.Rate = "110"
 	projectUserRates = append(projectUserRates, userrate)
 
 	userrate.User = "Sudheer"
@@ -160,10 +167,114 @@ func (t *SimpleChaincode) initializeData(stub shim.ChaincodeStubInterface, args 
 	projectUserRates = append(projectUserRates, userrate)
 
 	jsonAsBytes, _ = json.Marshal(projectUserRates)
+	//initialize user rates for Proj1
 	err = stub.PutState("Proj1", jsonAsBytes)
 	if err != nil {
 		return nil, err
 	}
 
+	//Initilizing the Project user rates p1 -->[ {u1 100}, {u2 200 }]
+	projectUserRates = []UserRate{}
+	userrate = UserRate{}
+	userrate.User = "Chandra"
+	userrate.Rate = "105"
+	projectUserRates = append(projectUserRates, userrate)
+
+	userrate.User = "Sudheer"
+	userrate.Rate = "110"
+	projectUserRates = append(projectUserRates, userrate)
+
+	userrate.User = "Sanjay"
+	userrate.Rate = "75"
+	projectUserRates = append(projectUserRates, userrate)
+
+	jsonAsBytes, _ = json.Marshal(projectUserRates)
+	//initialize user rates for Proj2
+	err = stub.PutState("Proj2", jsonAsBytes)
+	if err != nil {
+		return nil, err
+	}
+
+//initialize user rates for Proj3
+	err = stub.PutState("Proj3", jsonAsBytes)
+	if err != nil {
+		return nil, err
+	}
+
 	return nil, nil
+}
+
+func (t *SimpleChaincode) EnterResourceTime(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	//       0              1         2        3
+	// "ProjectName", "TaskName", "User", "QuantityInHours"
+	var rate int
+  var hours int
+
+	if len(args) != 4 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 4")
+	}
+
+	//input sanitation
+	fmt.Println("- start init acount")
+	if len(args[0]) <= 0 {
+		return nil, errors.New("1st argument ProjectName must be a non-empty string")
+	}
+	if len(args[1]) <= 0 {
+		return nil, errors.New("2nd argument TaskName must be a non-empty string")
+	}
+	if len(args[2]) <= 0 {
+		return nil, errors.New("3rd argument User must be a non-empty string")
+	}
+	if len(args[3]) <= 0 {
+		return nil, errors.New("4th argument QuantityInHours must be a non-empty string")
+	}
+
+ timeEntry := TimeEntry{}
+ timeEntry.ProjectName = args[0]
+ timeEntry.TaskName = args[1]
+ timeEntry.User = args[2]
+ timeEntry.QuantityInHours = args[3]
+ timeEntry.TotalAmount = "0"
+// derive amount
+
+	projectUsersAsBytes, err := stub.GetState(args[0])
+	if err != nil {
+		return nil, errors.New("Failed to get project user rates")
+	}
+	projectUserRates := []UserRate{}
+	json.Unmarshal(projectUsersAsBytes, &projectUserRates)
+
+	for i := range projectUserRates{
+		if strings.ToLower(projectUserRates[i].User) == strings.ToLower(args[2]) {
+			hours,_ = strconv.Atoi(args[3])
+			rate,_ = strconv.Atoi(projectUserRates[i].Rate)
+       timeEntry.TotalAmount = strconv.Itoa(hours * rate )
+		}
+	}
+
+//get time entires for user and project
+projectUserTimeEntryAsBytes, err := stub.GetState(args[0]+":::"+args[2])
+if err != nil {
+	return nil, errors.New("Failed to get project user time entry")
+}
+
+allProjectTimeEntries := []TimeEntry{}
+json.Unmarshal(projectUserTimeEntryAsBytes, &allProjectTimeEntries)
+
+//add current time entry to exisitng
+allProjectTimeEntries = append(allProjectTimeEntries, timeEntry)
+
+//put back all time entries
+jsonAsBytes, _ := json.Marshal(allProjectTimeEntries)
+err = stub.PutState(args[0]+":::"+args[2], jsonAsBytes)
+if err != nil {
+	return nil, err
+}
+
+	return nil,nil
+}
+
+func (t *SimpleChaincode) CompleteProjectMilestone(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+
+	return nil,nil
 }
